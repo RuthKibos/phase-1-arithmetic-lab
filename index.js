f15cee8158c1b4bafb3b1143aa10d0c67cdb8fbf
RuthKibos <kibosruth@gmail.com>
2024-03-24 00:22:10 +0300

    completed the Lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; // Generates a random integer greater than 0
+let num3 = 40;
+let num4 = 6;
+let mod = num3 % num4; 
+let max = Math.max(5, 10, 15, 20); 
